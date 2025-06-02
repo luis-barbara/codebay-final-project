3d595ef905db7f765a89ca74cdfae4b7d0a8452f
Luís Bárbara <150970169+luis-barbara@users.noreply.github.com>
2025-06-02 14:43:56 +0100

    Update urls.py

--- codebay/urls.py
+++ codebay/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api", include("marketplace.urls")),
+    path("api/", include("marketplace.urls")),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
