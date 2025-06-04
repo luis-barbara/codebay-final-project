5b8a9af76d193c6d3603e8732ca1c3afd817c848
Luís Bárbara <150970169+luis-barbara@users.noreply.github.com>
2025-06-04 09:25:12 +0100

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,10 +6,13 @@ from .models import User
 
 class UserAdmin(BaseUserAdmin):
     
-    list_display = ('username', 'email', 'full_name', 'is_staff', 'is_active')
+    # Colunas no painel de usuários
+    list_display = ('username', 'email', 'full_name', 'is_staff', 'is_active', 'last_login')
     
-    list_filter = ('is_staff', 'is_active')
-   
+    # Filtros para facilitar a pesquisa
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    
+    # Exibição detalhada das informações do utilizador
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         ('Personal info', {'fields': ('full_name', 'email', 'avatar', 'description', 'phone', 'position', 'location', 'website', 'github_account', 'rating')}),
@@ -17,14 +20,25 @@ class UserAdmin(BaseUserAdmin):
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
     
+    # Campos quando um novo utilizador for adicionado
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'full_name', 'email', 'password1', 'password2', 'is_staff', 'is_active')}
-        ),
+            'fields': ('username', 'full_name', 'email', 'password1', 'password2', 'is_staff', 'is_active')
+        }),
     )
+    
     search_fields = ('username', 'email', 'full_name')
+    
     ordering = ('username',)
 
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['password1'].widget.attrs['autocomplete'] = 'new-password'
+        form.base_fields['password2'].widget.attrs['autocomplete'] = 'new-password'
+        return form
+
 admin.site.register(User, UserAdmin)
+
 
