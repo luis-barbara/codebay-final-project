53c55e18c4c3449e54273dc67dfb881bfd20f524
Luís Bárbara <150970169+luis-barbara@users.noreply.github.com>
2025-06-04 09:46:49 +0100

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,8 +6,7 @@ from .views import SignupView, PublicProfileView, SigninView
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('signin/', SigninView.as_view(), name='signin'),
-    path('profile/<str:email>/', PublicProfileView.as_view(), name='public_profile'),
-    path('accounts/', include('allauth.urls')), 
+    path('profile/<str:email>/', PublicProfileView.as_view(), name='public_profile')
 ]
 
 
