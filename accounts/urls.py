3253437ff36163222a0b4f9af2e26dc4196cfa7d
Luís Bárbara <150970169+luis-barbara@users.noreply.github.com>
2025-06-04 09:27:21 +0100

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,16 @@
 # accounts/urls.py
 
-from django.urls import path
+from django.urls import path, include  
 from .views import SignupView, PublicProfileView, SigninView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('signin/', SigninView.as_view(), name='signin'),
-    path('profile/<str:username>/', PublicProfileView.as_view(), name='public_profile'),
+    path('profile/<str:email>/', PublicProfileView.as_view(), name='public_profile'),
+    path('accounts/', include('allauth.urls')), 
 ]
+
+
+
+
 
