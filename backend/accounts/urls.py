24e7f333b600e1895a37477f03025a813f15af15
Luís Bárbara <150970169+luis-barbara@users.noreply.github.com>
2025-06-17 10:08:12 +0100

    Update urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,12 +3,15 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import SignupView, PublicProfileView, TokenObtainPairView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('profile/<str:email>/', PublicProfileView.as_view(), name='public_profile'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('oauth/google/', RedirectView.as_view(url='/accounts/google/login/'), name='google_login_redirect'),
+    path('oauth/github/', RedirectView.as_view(url='/accounts/github/login/'), name='github_login_redirect'),
 ]
 
 
