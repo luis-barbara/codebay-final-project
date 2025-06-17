62d21cbe42e1b766089c35fdcc2baa18cd3cfd96
Luís Bárbara <150970169+luis-barbara@users.noreply.github.com>
2025-06-17 10:35:48 +0100

    Update urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import SignupView, PublicProfileView, TokenObtainPairView
 from django.views.generic import RedirectView
+from .views import OAuthRedirectView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
